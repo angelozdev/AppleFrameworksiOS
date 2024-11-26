21d083fd896680a8662a4f4152a5970facbd42d6
Angelo Zambrano <angelozam17@gmail.com>
2024-11-26 07:04:51 -0500

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Angelo Zambrano on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
